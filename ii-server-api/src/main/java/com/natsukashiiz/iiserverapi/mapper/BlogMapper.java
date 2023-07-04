e766cf2164d14e2bb204342ce75a99f31172ba10
vv2dev <74323401+natsukashiiz@users.noreply.github.com>
2023-07-04 17:52:16 +0700

    Update BlogMapper.java

--- ii-server-api/src/main/java/com/natsukashiiz/iiserverapi/mapper/BlogMapper.java
+++ ii-server-api/src/main/java/com/natsukashiiz/iiserverapi/mapper/BlogMapper.java
@@ -1,11 +1,20 @@
 package com.natsukashiiz.iiserverapi.mapper;
 
+import com.natsukashiiz.iicommon.model.Pagination;
 import com.natsukashiiz.iiserverapi.entity.Blog;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface BlogMapper {
+    List<Blog> findAll();
+
     Optional<Blog> findById(Long id);
+
+    List<Blog> findByUid(Long uid);
+
+    List<Blog> findBy(@Param("entity") Blog entity, @Param("page") Pagination page);
 }
